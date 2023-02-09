0a7cc104a840e3fa4f9903c7f34e0dc6eb7e64fe
Breno <bwbreno16@gmail.com>
2023-02-09 10:06:49 -0300

    Parte do front-end montado, já com cadastro através do Google

---
+++ web/src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="home">
+
+    </div>
+</template>
+
+<script lang="ts">
+    export default{
+        name: "home"
+    }
+</script>
+
+<style scoped>
+    #home {
+        background-image: url("/img/death_star.jpg");
+    }
+</style>
